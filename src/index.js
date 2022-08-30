7d6dff1120bd1942baf4d3a9cd596ccd9bd6ad07
KP <kpinc425@gmail.com>
2022-08-30 15:41:30 -0700

    Cleanup code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { random } from 'lodash';
 import './style.css';
 
 const img = document.querySelector('img');
@@ -6,35 +7,10 @@ const inputSearchText = document.querySelector('.inputSearch');
 const btnSearch = document.querySelector('#btnSearch');
 
 
-fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'})
-.then(function(response) {
-    return response.json();
-})
-.then(function(response) {
-    console.log(response.data.images.original.url);
-    img.src = response.data.images.original.url;
-})
-.catch(function(error) {
-    console.log(error);
-    img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-});
 
+randomFetch();
 
-btnRandomImg.addEventListener('click', () => {
-    console.log('click');
-    fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.data.images.original.url);
-        img.src = response.data.images.original.url;
-    })
-    .catch(function(error) {
-        console.log(error);
-        img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-    });
-})
+btnRandomImg.addEventListener('click', randomFetch)
 
 btnSearch.addEventListener('click', () => {
     console.log(inputSearchText.value);
@@ -52,3 +28,17 @@ btnSearch.addEventListener('click', () => {
         });
 })
 
+function randomFetch(){
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response.data.images.original.url);
+            img.src = response.data.images.original.url;
+        })
+        .catch(function(error) {
+            console.log(error);
+            img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
+        });
+}
