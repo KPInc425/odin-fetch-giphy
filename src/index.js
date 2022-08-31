86eef18534ad8ad44410645bfbd4869ae73f9d4b
KP <kpinc425@gmail.com>
2022-08-31 15:05:54 -0700

    Clean up code, add simple error handling

--- src/index.js
+++ src/index.js
@@ -22,25 +22,25 @@ btnCopyLink.addEventListener('click', () => {
 })
 
 async function randomFetch(){
-    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'});
-    const randomData = await response.json();
-    console.log(randomData.data.images.original.url);
-    img.src = randomData.data.images.original.url;
-
-    // .catch(function(error) {
-    //     console.log(error);
-    //     img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-    // });
+    try {
+        const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'});
+        const randomData = await response.json();
+        console.log(randomData.data.images.original.url);
+        img.src = randomData.data.images.original.url;
+    } catch (error) {
+        console.log(error);
+        img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
+    }
 }
 
 async function searchFetch() {
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i&s=' + inputSearchText.value, {mode: 'cors'});
-    const searchData = await response.json()
-    console.log(searchData.data.images.original.url);
-    img.src = searchData.data.images.original.url;
-
-        // .catch(function(error) {
-        //     console.log(error);
-        //     img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-        // });
+    try {
+        const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i&s=' + inputSearchText.value, {mode: 'cors'});
+        const searchData = await response.json()
+        console.log(searchData.data.images.original.url);
+        img.src = searchData.data.images.original.url;
+    } catch (error) {
+        console.log(error);
+        img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
+    }
 }
