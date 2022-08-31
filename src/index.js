36e92481ef14bfef9801694ba7684b5fa2250117
KP <kpinc425@gmail.com>
2022-08-31 15:05:12 -0700

    Bugfix copylink button

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-import { random } from 'lodash';
 import './style.css';
 
 const img = document.querySelector('img');
 const btnRandomImg = document.querySelector('#btnRandomImg');
 const inputSearchText = document.querySelector('.inputSearch');
 const btnSearch = document.querySelector('#btnSearch');
+const btnCopyLink = document.querySelector('#btnCopyLink');
 
 
 
 randomFetch();
+// random button click fetches random gif
+btnRandomImg.addEventListener('click', randomFetch);
 
-btnRandomImg.addEventListener('click', randomFetch)
 
-btnSearch.addEventListener('click', () => {
-    console.log(inputSearchText.value);
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i&s=' + inputSearchText.value, {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            console.log(response.data.images.original.url);
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error) {
-            console.log(error);
-            img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-        });
+// gets value from input for search term in fetch 
+btnSearch.addEventListener('click', searchFetch);
+inputSearchText.addEventListener('keyup', searchFetch);
+btnCopyLink.addEventListener('click', () => {
+    navigator.clipboard.writeText(img.src);
+    console.log("Copied the Link!");
 })
 
-function randomFetch(){
-    fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            console.log(response.data.images.original.url);
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error) {
-            console.log(error);
-            img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
-        });
+async function randomFetch(){
+    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i', {mode: 'cors'});
+    const randomData = await response.json();
+    console.log(randomData.data.images.original.url);
+    img.src = randomData.data.images.original.url;
+
+    // .catch(function(error) {
+    //     console.log(error);
+    //     img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
+    // });
+}
+
+async function searchFetch() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=0JvtDfunbRZAukBB5R94oBnUtDprGS0i&s=' + inputSearchText.value, {mode: 'cors'});
+    const searchData = await response.json()
+    console.log(searchData.data.images.original.url);
+    img.src = searchData.data.images.original.url;
+
+        // .catch(function(error) {
+        //     console.log(error);
+        //     img.src = 'https://media4.giphy.com/media/l0HUjDphISoMMCMPm/giphy.gif?cid=bbfc1e498djkmm9q38jdt5tdzvq5a41cf28x7z2cfyw4m6q7&rid=giphy.gif&ct=g';
+        // });
 }
